781dc0a1bf9671b7f566185277127fafdf0bb8fa
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-04-18 15:33:53 +0600

    Video Section Responsive

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,14 +4,14 @@ import thumbnail from "../assets/Images/Video Thumbnail.png";
 
 const Video = () => {
     return (
-        <section className="bg-[#d5f0f2] py-32 px-5">
-            <div className="bg-main relative h-[600px]">
-                <div className=" absolute top-1/2 left-1/2 translate-[-50%] w-full">
+        <section className="bg-[#d5f0f2] py-12 min-[420px]:py-20 min-[500px]:py-28 sm:py-32">
+            <div className="bg-main relative h-[120px] min-[365px]:h-[160px] min-[440]:h-[200px] min-[635px]:h-[300px] min-[960px]:h-[460px] xl:h-[600px]">
+                <div className=" absolute top-1/2 left-1/2 translate-[-50%] w-full px-5">
                     <div className="relative">
                         <img src={thumbnail} alt="video thumbnail" className="mx-auto" />
-                        <h2 className="text-5xl font-bold text-center absolute top-10 left-1/2 translate-x-[-50%]">Plan a Beautiful Personalized <br /> Digital Remembrance</h2>
-                        <button className=" rounded-full bg-main p-6 absolute bottom-0 left-1/2 translate-x-[-50%] translate-y-[50%] cursor-pointer hover:bg-second">
-                            <FaPlay size={24} />
+                        <h2 className="text-sm sm:text-xl md:text-3xl lg:text-5xl font-bold text-center absolute top-4 sm:top-10 left-1/2 translate-x-[-50%] w-full">Plan a Beautiful Personalized <br /> Digital Remembrance</h2>
+                        <button className=" rounded-full bg-main p-3 sm:p-6 absolute bottom-0 left-1/2 translate-x-[-50%] translate-y-[50%] cursor-pointer hover:bg-second">
+                            <FaPlay className="text-lg sm:text-xl" />
                         </button>
                     </div>
                 </div>
